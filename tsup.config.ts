5ad282ed52b4bcbd9b3df1dad8b681d2339f0c2f
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-10-17 21:46:50 +0200

    Added target to tsup config
    closes #12

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => {
     return {
+        target: "node14",
         minify: !options.watch,
         entry: ['src/index.ts'],
         clean: true,
